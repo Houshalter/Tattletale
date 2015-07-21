672ff27190edc69b58e07d51dc46c8f3c4bfaa34
Houshalter <Houshalter@gmail.com>
2015-07-21 02:50:39 -0400

    config added

--- AuditBot.lua
+++ AuditBot.lua
@@ -1,10 +1,11 @@
 package.path = "./?.lua;./?/init.lua;"
 
+require 'config'
 require "irc"
 require 'markovDetect'
 local sleep = require "socket".sleep
 
-local s = irc.new{nick = "AuditBot"}
+local s = irc.new{nick = }
 
 s:hook("OnChat", function(user, channel, message)
 	print(("[%s] %s: %s"):format(channel, user.nick, message))
@@ -15,6 +16,8 @@ function markovDetect(user, channel)
 	local p = bayes(nick,1/10)
 	if p > 0.5 then 
 		s:sendChat(channel, string.format("%s is a bot! %g probabilty.", nick, p))
+	else
+		s:send("mode #lesswrong +v nick")
 	end
 	print(string.format("Joined: %s. Bayes: %g, Probability Real: %g, Probability Fake: %g", nick, p, probability(nick), probFake(nick)))
 	print(("username: %s, host: %s, realname: %s"):format(tostring(user.username), tostring(user.host), tostring(user.realname)))
@@ -23,11 +26,13 @@ end
 s:hook("OnJoin", markovDetect)
 
 
-s:connect("chat.freenode.net")
-s:join("#lesswrong")
-s:join("#lw-bots")
+s:connect(server)
+
+for i, channel in ipairs(channels) do
+	s:join(channel)
+end
 
 while true do
 	s:think()
-	sleep(0.1)
+	sleep(refeshRate)
 end
---
+++ conflig.lua
@@ -0,0 +1,4 @@
+refeshRate = 0.5
+nick = "AuditBot"
+server = "chat.freenode.net"
+channels = {"#lesswrong", "#lw-bots"}
