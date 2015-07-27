f0afa68ff1feacc90025b492d1d6ec8869ae0f48
Houshalter <Houshalter@gmail.com>
2015-07-26 20:06:30 -0400

    cleaned up code

--- markovDetect.lua
+++ markovDetect.lua
@@ -1,5 +1,5 @@
+--we start with special characters representing the start of a name, end of name, and non-letters
 local validChars = {string.char(0),string.char(1),string.char(2)}
---for i = 65, 122 do table.insert(validChars, string.char(i)) end
 local a,z = string.byte('a'),string.byte('z')
 for i = a, z do table.insert(validChars, string.char(i)) end
 
@@ -13,16 +13,10 @@ end
 percentNamesLower = lowerCaseNames/#names
 
 
+--replace anything not a lower case letter with special character
+--then appends starting and ending symbols to it.
 function process(n)
-	local result = ''
-	for i = 1, #n do
-		local c = n:sub(i,i)
-		if (c:byte() < a) or (c:byte() > z) then
-			c = string.char(2)
-		end
-		result = result..c
-	end
-	return string.char(0)..result..string.char(1)
+	return string.char(0)..n:gsub('[^%l]', string.char(2))..string.char(1)
 end
 
 
@@ -32,6 +26,8 @@ end
 
 totalCharCount = 0
 charCount = {}
+
+--Here we build a table that represents how often each character occurs
 for i, n in ipairs(names) do
 	n = process(n)
 	for i = 2, #n do
@@ -40,10 +36,11 @@ for i, n in ipairs(names) do
 	end
 end
 
-
+--this initializes a table that will represent how often each character occurs
+--after every other character. It starts with a prior, as if it had seen each
+--character combination once. Each combination is weighted by the frequency
+--that chracter occurs.
 local tableProbs = {}
-
-
 for i,char in ipairs(validChars) do
 	tableProbs[char] = {}
 	for z = 2,#validChars do
@@ -52,13 +49,15 @@ for i,char in ipairs(validChars) do
 	end	
 end
 
+--and now we start counting how often each character pair actually occurs
 for i,n in ipairs(names) do
 	n = process(n)
-	for i = 1, n:len()-1 do
+	for i = 1, #n-1 do
 		tableProbs[n:sub(i,i)][n:sub(i+1,i+1)] = tableProbs[n:sub(i,i)][n:sub(i+1,i+1)]+1
 	end
 end
 
+--this calculates the probability of a name being produced by a real user
 function probability(name)
 	local n = process(name)
 	local logProb = 0
@@ -68,7 +67,6 @@ function probability(name)
 		for c,num in pairs(tableProbs[n:sub(i,i)]) do
 			total = total + num
 		end
-		--print(n:sub(i+1,i+1), nChar, total)
 		logProb = logProb + math.log(nChar/total)
 	end
 	return logProb
@@ -77,13 +75,11 @@ end
 --[[Olivia notes that in python this would be:
 def probfake(n): if n.isalpha(): return math.log(1/26)*len(n)+math.log(1/14); else: return 0 - sys.maxint
 
-I note that if I remembered how to regex this could be reduced about as much in just lua.]]
+I note that if I remembered how to regex this could be reduced about as much in just lua.
+
+EDIT: much better : )  ]]
 function probFake(n)
-	for i = 1, #n do
-		if (string.byte(n:sub(i,i)) < a) or (string.byte(n:sub(i,i)) > z) then
-			return -math.huge
-		end
-	end
+	if n:match('[^%l]') then return -math.huge end
 	return math.log(1/26)*n:len()+math.log(1/14)
 end
 
@@ -92,15 +88,6 @@ function bayes(n,p)
 	p = p/((1-p)*percentNamesLower+p)
 	local r = math.exp(probability(n))
 	local f = math.exp(probFake(n))
-	--return math.exp(f+math.log(p)-math.log(math.exp(r)))
-	
 	--bayes theorem
 	return f*p/(f*p+r*(1-p))
 end
-
---[[for i,n in ipairs(names) do
-	print(n, bayes(n,1/20))
-end
-local p = 1/20
-print(percentNamesLower, p, p/((1-p)*percentNamesLower+p))
-print(bayes("zgty", p))]]
